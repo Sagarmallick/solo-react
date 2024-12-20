aa084a0767ae1e0380fde1bbc487277e9b97de8e
sagar mallick <sagarmallick@sagars-MacBook-Air.local>
2024-12-21 03:13:26 +0530

    session-3

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "render h1 with the div element"),
-    React.createElement("h1", {}, "render h2 with the div element"),
-  ])
+//JSX( transpile before it reches to js) - parcel - babel
+//JSX => bebel transpileit to Rect.createElement  =>ReactElement-js object  => HTML(render)
+
+const jsxheading = <h1 className="head">React Element</h1>;
+
+const Title = () => <h1>solo react for JSX</h1>;
+
+const HeadingComponent = () => (
+  <div id="container">
+    {jsxheading}
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1>functional component </h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
